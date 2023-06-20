318b0a8ac1f77f032c4356478e5b244dbf0b1d70
Abhishek Reddy Ammireddy <abhishekreddy199189@gmail.com>
2023-06-21 00:03:36 +0530

    frontend

---
+++ frontend.sh
@@ -0,0 +1,15 @@
+yum install nginx -y
+
+rm -rf /usr/share/nginx/html/*
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip
+
+## We need to copy config file here
+
+systemctl enable nginx
+
+systemctl start nginx
