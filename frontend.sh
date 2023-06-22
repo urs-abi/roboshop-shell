ef95f5b31860ddac0048e6299381acaded7099e8
Abhishek Reddy Ammireddy <abhishekreddy199189@gmail.com>
2023-06-22 16:29:40 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -7,12 +7,11 @@ rm -rf /usr/share/nginx/html/* &>>/tmp/logfile
 echo -e "\e[32m Downloading package \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/logfile
 
+echo -e "\e[32m Extracting Frontend package \e[0m"
 cd /usr/share/nginx/html 
-
-echo -e "\e[32m Unzipping package \e[0m"
 unzip /tmp/frontend.zip &>>/tmp/logfile
 
-echo -e "\e[32m Copying Configuration File \e[0m"
+echo -e "\e[32m Updating Configuration File \e[0m"
 cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/logfile
 
 echo -e "\e[32m Starting Nginx Service \e[0m"
