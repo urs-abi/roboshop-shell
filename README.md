4b8c4345e8ffae93d12d25d33d57bcfa536f38a5
Abhishek Reddy Ammireddy <abhishekreddy199189@gmail.com>
2023-06-22 16:42:10 +0530

    mongodb

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # README
 
+# git clone https://github.com/urs-abi/roboshop-shell.git
+
 # Home Directory - /home/centos/roboshop-shell/
 
 # Print Green Statement - echo -e "\e[32m  \e[0m"
--- mongodb.sh
+++ mongodb.sh
@@ -5,7 +5,7 @@ echo -e "\e[32m Installing MongoDB \e[0m"
 yum install mongodb-org -y &>>/tmp/logfile
 
 echo -e "\e[32m Mondifying MongoDB Conf File \e[0m"
-sed -i ‘s/127.0.0.1/0.0.0.0/’ /etc/mongo.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongo.conf
 
 echo -e "\e[32m Starting MongoDB Service \e[0m"
 systemctl enable mongod &>>/tmp/logfile
