3a2af47f1c6b75d1b9fa76fd74c6d2c73e4d7984
Abhishek Reddy Ammireddy <abhishekreddy199189@gmail.com>
2023-06-23 00:25:12 +0530

    rabbitmq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -7,10 +7,10 @@ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/sc
 echo -e "\e[32m Installing RabbitMQ \e[0m"
 yum install rabbitmq-server -y &>>/tmp/logfile
 
-echo -e "\e[32m Configuring Application User \e[0m"
-rabbitmqctl add_user roboshop roboshop123 &>>/tmp/logfile
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/logfile
-
 echo -e "\e[32m Starting RabbitMQ Service \e[0m"
 systemctl enable rabbitmq-server &>>/tmp/logfile
 systemctl restart rabbitmq-server &>>/tmp/logfile
+
+echo -e "\e[32m Configuring Application User \e[0m"
+rabbitmqctl add_user roboshop roboshop123 &>>/tmp/logfile
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/logfile
