6123a821bac28d36cea6b2017d04ce9cd7cbf34a
Abhishek Reddy Ammireddy <abhishekreddy199189@gmail.com>
2023-06-22 16:37:09 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,13 @@
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+echo -e "\e[32m Creating MongoDB Repo File \e[0m"
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/logfile
 
-yum install mongodb-org -y 
+echo -e "\e[32m Installing MongoDB \e[0m"
+yum install mongodb-org -y &>>/tmp/logfile
 
-## Modify config file
+echo -e "\e[32m Mondifying MongoDB Conf File \e[0m"
+sed -i ‘s/127.0.0.1/0.0.0.0/’ /etc/mongo.conf
 
-systemctl enable mongod
+echo -e "\e[32m Starting MongoDB Service \e[0m"
+systemctl enable mongod &>>/tmp/logfile
 
-systemctl start mongod
+systemctl restart mongod &>>/tmp/logfile
