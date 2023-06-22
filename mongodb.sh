b9a1f627c550801d5506b68f7f9e5206debb956a
Abhishek Reddy Ammireddy <abhishekreddy199189@gmail.com>
2023-06-22 16:53:41 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -5,7 +5,7 @@ echo -e "\e[32m Installing MongoDB \e[0m"
 yum install mongodb-org -y &>>/tmp/logfile
 
 echo -e "\e[32m Mondifying MongoDB Conf File \e[0m"
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[32m Starting MongoDB Service \e[0m"
 systemctl enable mongod &>>/tmp/logfile
