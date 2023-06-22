979614b83a4f7bb90cb60f7423aa23d3a4043e68
Abhishek Reddy Ammireddy <abhishekreddy199189@gmail.com>
2023-06-22 17:13:07 +0530

    catalogue

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 echo -e "\e[32m Creating MongoDB Repo File \e[0m"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/logfile
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/logfile
 
 echo -e "\e[32m Installing MongoDB \e[0m"
 yum install mongodb-org -y &>>/tmp/logfile
