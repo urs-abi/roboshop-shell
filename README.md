f5938073a277e9e7566221c75ac70d54486d3114
Abhishek Reddy Ammireddy <abhishekreddy199189@gmail.com>
2023-06-22 18:45:10 +0530

    mysql2

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 # git clone https://github.com/urs-abi/roboshop-shell.git
 
-# Home Directory - /home/centos/roboshop-shell/
+# Home Directory -> /home/centos/roboshop-shell/
 
-# Print Green Statement - echo -e "\e[32m  \e[0m"
+# Print Green Statement -> echo -e "\e[32m  \e[0m"
+
+# Output Redirectos -> &>>/tmp/logfile
--- mysql.sh
+++ mysql.sh
@@ -1,15 +1,15 @@
 echo -e "\e[32m Disbaling current MySQL \e[0m"
-yum module disable mysql -y 
+yum module disable mysql -y &>>/tmp/logfile
 
 echo -e "\e[32m Creating MySQL Repo \e[0m"
-cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/logfile
 
 echo -e "\e[32m Installing MySQL \e[0m"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>/tmp/logfile
 
 echo -e "\e[32m Setting Root Password \e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/logfile
 
 echo -e "\e[32m Starting MySQL Service \e[0m"
-systemctl enable mysqld
-systemctl start mysqld  
+systemctl enable mysqld &>>/tmp/logfile
+systemctl start mysqld &>>/tmp/logfile
